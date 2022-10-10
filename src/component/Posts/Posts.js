5cf2c74e504bcec2c93e80624e050b0502c286b7
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-10 23:10:55 +0530

    backgoured , post, posts coponent's style added

--- src/component/Posts/Posts.js
+++ src/component/Posts/Posts.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./Post/Post";
-// import useStyles from "./styles";
+import useStyles from "./styles";
 
 const Posts = () => {
-  const postdata = useSelector((state) => state.posts);
-  console.log(postdata);
-  // const classes = useStyles();
-  return (
-    <div>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </div>
+  const posts = useSelector((state) => state.posts);
+  console.log(posts);
+  const classes = useStyles();
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid className={classes.mainContainer} container alignContent="stretch" spacing={3}>
+      {posts.map((post) => {
+        return (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 };
 
