19f2d783f7aa1f2d9fed86112d91ef5a35b15623
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-11 18:51:34 +0530

    google auth

--- src/component/Auth/Auth.js
+++ src/component/Auth/Auth.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Paper, Avatar, Typography, Button, Grid } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-
+import { GoogleLogin } from "react-google-login";
 import useStyles from "./styles";
 import Input from "./Input";
+import Icon from "./icon";
+import { gapi } from "gapi-script";
 
 const Auth = () => {
   const classes = useStyles();
@@ -18,6 +20,21 @@ const Auth = () => {
   const switchMode = () => {
     setIsSignUp((isSignup) => !isSignup);
   };
+
+  const googleSuccess = (res) => {
+    console.log(res);
+  };
+  const googleError = (error) => {
+    console.log(error);
+    console.log("google authontication failure");
+  };
+
+  useEffect(() => {
+    gapi.load("client:auth2", () => {
+      gapi.auth2.init({ clientId: "871097789638-bhcfgs4b58vu9t9gmnq23fkiqhgl5khr.apps.googleusercontent.com" });
+    });
+  }, []);
+
   return (
     <Container component="main" maxWidth="xs">
       <Paper className={classes.paper} elevation={3}>
@@ -42,8 +59,19 @@ const Auth = () => {
           <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
             {isSignup ? "Sign Up" : "Sign In"}
           </Button>
+          <GoogleLogin
+            clientId="871097789638-bhcfgs4b58vu9t9gmnq23fkiqhgl5khr.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
+                Google Sign In
+              </Button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleError}
+            cookiePolicy="single_host_origin"
+          />
 
-          <Grid container justify="flex-end">
+          <Grid container justifyContent="flex-end">
             <Grid item>
               <Button onClick={switchMode}>{isSignup ? "Already have an account? Sign in" : "Don't have an account? Sign Up"}</Button>
             </Grid>
