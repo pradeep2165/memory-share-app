a48eceb0a4c4fd0bf461c7e2eb7e894b3200893e
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-11 08:28:03 +0530

    mobile responsive

--- src/App.js
+++ src/App.js
@@ -15,24 +15,24 @@ const App = () => {
   useEffect(() => {
     dispatch(getPosts());
   }, [dispatch, currentId]);
+
   return (
     <Container maxWidth="lg">
-      <AppBar className="classes.appBar" position="static" color="inherit">
+      <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">
           Memories
-          <img className={classes.image} src={memories} alt="memories" height="60" />
         </Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3} className={classes.mainContainer}>
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
               <Form setCurrentId={setCurrentId} currentId={currentId} />
             </Grid>
-            <Grid></Grid>
           </Grid>
         </Container>
       </Grow>
