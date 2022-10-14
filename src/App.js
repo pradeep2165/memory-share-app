469816b10ddcee66320a29bdfd17219f09c6df6a
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-14 14:15:31 +0530

    authontication not required if user exist

--- src/App.js
+++ src/App.js
@@ -7,16 +7,24 @@ import Auth from "./component/Auth/Auth";
 import PostDetails from "./component/PostDetails/PostDetails";
 
 const App = () => {
+  const Authorise = () => {
+    const user = JSON.parse(localStorage.getItem("profile"));
+    if (user) {
+      return <Navigate to="/posts" />;
+    } else {
+      return <Auth />;
+    }
+  };
   return (
     <BrowserRouter>
       <Container maxWidth="lg">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Navigate to="/posts" replace />} />
+          <Route path="/" element={<Navigate to="/posts" />} />
           <Route path="/posts" element={<Home />} />
           <Route path="/posts/search" element={<Home />} />
           <Route path="/posts/:id" element={<PostDetails />} />
-          <Route path="/auth" element={<Auth />} />
+          <Route exact path="/auth" element={<Authorise />} />
         </Routes>
       </Container>
     </BrowserRouter>
