93a5a3bb1d100f5b6f15851f5b1c7091a56076ab
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-14 15:58:59 +0530

    recomanded post working fine

--- src/component/PostDetails/PostDetails.js
+++ src/component/PostDetails/PostDetails.js
@@ -8,10 +8,10 @@ import moment from "moment";
 import CommentSection from "./CommentSection";
 
 const PostDetails = () => {
-  const { post, isLoading } = useSelector((state) => state.posts);
+  const { post, posts, isLoading } = useSelector((state) => state.posts);
   const classes = useStyles();
   const { id } = useParams();
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   console.log(post);
 
@@ -27,7 +27,7 @@ const PostDetails = () => {
     // eslint-disable-next-line
   }, [post]);
   if (!post) return null;
-
+  const openPost = (_id) => navigate(`/posts/${_id}`);
   if (isLoading) {
     return (
       <Paper elevation={6} className={classes.loadingPaper}>
@@ -35,7 +35,7 @@ const PostDetails = () => {
       </Paper>
     );
   }
-
+  const recommendedPosts = posts.filter(({ _id }) => _id !== post._id);
   return (
     <Paper style={{ padding: "20px", borderRadius: "15px" }} elevation={6}>
       <div className={classes.card}>
@@ -68,6 +68,33 @@ const PostDetails = () => {
           <img className={classes.media} src={post.selectedFile || "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"} alt={post.title} sx={{ md: { maxWidth: "720px" } }} />
         </div>
       </div>
+      {!!recommendedPosts.length && (
+        <div className={classes.section}>
+          <Typography gutterBottom variant="h5">
+            You might also like:
+          </Typography>
+          <Divider />
+          <div className={classes.recommendedPosts}>
+            {recommendedPosts.map(({ title, name, message, likes, selectedFile, _id }) => (
+              <div style={{ margin: "20px", cursor: "pointer" }} onClick={() => openPost(_id)} key={_id}>
+                <Typography gutterBottom variant="h6">
+                  {title}
+                </Typography>
+                <Typography gutterBottom variant="subtitle2">
+                  {name}
+                </Typography>
+                <Typography gutterBottom variant="subtitle2">
+                  {message}
+                </Typography>
+                <Typography gutterBottom variant="subtitle1">
+                  Likes: {likes.length}
+                </Typography>
+                <img src={selectedFile} width="200px" alt="" />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </Paper>
   );
 };
